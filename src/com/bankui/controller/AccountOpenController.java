faf1ba8db3bce488e743ab8def42269cf2285621
Patil <harsha.a.patil@capgemini.com>
2022-01-09 14:51:26 +0530

    initial commit

---
+++ src/com/bankui/controller/AccountOpenController.java
@@ -0,0 +1,18 @@
+package com.bankui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AccountOpenController {
+
+	@RequestMapping("/")
+	public String showAccountOpenScreen() {
+		return "account_open";
+	}
+	
+	@RequestMapping("/login")
+	public String showLoginScreen() {
+		return "account_open";
+	}
+}
